3fe4bc4fb7ab15a20d7a1786242bbc452b5256d7
ginger-kang <kdhoon07@gmail.com>
2020-04-03 03:13:04 +0900

    게시판 구현

--- src/main/java/com/kang/ginger/springboot/service/PostsService.java
+++ src/main/java/com/kang/ginger/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kang.ginger.springboot.service;
 
 import com.kang.ginger.springboot.domain.posts.Posts;
 import com.kang.ginger.springboot.domain.posts.PostsRepository;
+import com.kang.ginger.springboot.web.Dto.PostsListResponseDto;
 import com.kang.ginger.springboot.web.Dto.PostsResponseDto;
 import com.kang.ginger.springboot.web.Dto.PostsSaveRequestDto;
 import com.kang.ginger.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
