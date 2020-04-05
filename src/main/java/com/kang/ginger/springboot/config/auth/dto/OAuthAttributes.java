3989188877d95f7d1ae93f2c7b460e760a02e8b8
ginger-kang <kdhoon07@gmail.com>
2020-04-05 18:56:05 +0900

    네이버 로그인

--- src/main/java/com/kang/ginger/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kang/ginger/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,7 +28,22 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId
             , String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -37,7 +52,8 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName).build();
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public User toEntity() {
